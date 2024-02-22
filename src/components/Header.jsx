72e9b810537ed7144b016763f77581ad082fc941
adenirannoah1234 <adenirannoah9@gmail.com>
2024-02-22 18:26:40 +0100

    made some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = ({ category, title }) => {
   return (
     <div className="mb-10">
       <p className="text-gray-400">{category}</p>
-      <p className="text-3xl font-extrabold tracking-tight text-slate-900 mt-7">
+      <p className="text-7xl font-extrabold tracking-tight text-slate-900 md:mt-8 ">
         {title}
       </p>
     </div>
